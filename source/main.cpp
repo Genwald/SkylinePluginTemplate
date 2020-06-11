b89f3e757accc1524fe160ebb670b2a12012a719
Genwald <sethv27@gmail.com>
2020-06-10 19:01:15 -0700

    Add more comments to main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,9 @@
 #include "main.hpp"
 
+// function definition of the original function. Function pointer is given by A64HookFunction when used
 void (*originalNpadStateHandheldFunc)(nn::hid::NpadHandheldState *state, u32 const &controllerID);
+// The function to be called in place of the function that is hooked
+// You can choose to call the original function at the beginning, the end, or not at all
 void GetNpadStateHandheldHook(nn::hid::NpadHandheldState *state, u32 const &controllerID) {
     originalNpadStateHandheldFunc(state, controllerID);
     if(state->Buttons & nn::hid::KEY_A) {
@@ -20,7 +23,7 @@ int main() {
         // look up the function by symbol because it is an overloaded function
         u64 GetNpadStateHandheldAddr;
         nn::ro::LookupSymbol(&GetNpadStateHandheldAddr, "_ZN2nn3hid12GetNpadStateEPNS0_17NpadHandheldStateERKj");
-        // Hook to log when the A button is pressed in hadnheld state
+        // Hook to log when the A button is pressed in handheld state
         A64HookFunction(
             reinterpret_cast<void*>(GetNpadStateHandheldAddr),
             reinterpret_cast<void*>(GetNpadStateHandheldHook),
