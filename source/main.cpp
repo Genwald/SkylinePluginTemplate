3c7fc08bea9afff9bb01a07eb79cd38e15195010
Genwald <sethv27@gmail.com>
2020-06-10 20:37:01 -0700

    fix indentation

--- source/main.cpp
+++ source/main.cpp
@@ -18,24 +18,24 @@ Result openFileHook(nn::fs::FileHandle *file, char const* path, s32 mode) {
 }
 
 int main() {
-        skyline::TcpLogger::SendRaw("HELLO FROM PLUGIN\n");
+    skyline::TcpLogger::SendRaw("HELLO FROM PLUGIN\n");
 
-        // look up the function by symbol because it is an overloaded function
-        u64 GetNpadStateHandheldAddr;
-        nn::ro::LookupSymbol(&GetNpadStateHandheldAddr, "_ZN2nn3hid12GetNpadStateEPNS0_17NpadHandheldStateERKj");
-        // Hook to log when the A button is pressed in handheld state
-        A64HookFunction(
-            reinterpret_cast<void*>(GetNpadStateHandheldAddr),
-            reinterpret_cast<void*>(GetNpadStateHandheldHook),
-            (void**) &originalNpadStateHandheldFunc
-        );
+    // look up the function by symbol because it is an overloaded function
+    u64 GetNpadStateHandheldAddr;
+    nn::ro::LookupSymbol(&GetNpadStateHandheldAddr, "_ZN2nn3hid12GetNpadStateEPNS0_17NpadHandheldStateERKj");
+    // Hook to log when the A button is pressed in handheld state
+    A64HookFunction(
+        reinterpret_cast<void*>(GetNpadStateHandheldAddr),
+        reinterpret_cast<void*>(GetNpadStateHandheldHook),
+        (void**) &originalNpadStateHandheldFunc
+    );
 
-        // This function has no overloads, so we don't need to look it up by its symbol
-        // Hook to log the name of every opened file
-        A64HookFunction(
-            reinterpret_cast<void*>(nn::fs::OpenFile),
-            reinterpret_cast<void*>(openFileHook),
-            (void**) &originalOpenFileFunc
-        );
+    // This function has no overloads, so we don't need to look it up by its symbol
+    // Hook to log the name of every opened file
+    A64HookFunction(
+        reinterpret_cast<void*>(nn::fs::OpenFile),
+        reinterpret_cast<void*>(openFileHook),
+        (void**) &originalOpenFileFunc
+    );
 
 }
