2d2f7fb390cdd98aa1abe2d21e6cbd6968af27bc
Connor Kurtz <connorkurtz22@gmail.com>
2020-09-30 00:08:59 -0700

    Added inline hook method, added utils::init implementation

--- include/skyline/inlinehook/And64InlineHook.hpp
+++ include/skyline/inlinehook/And64InlineHook.hpp
@@ -1,1 +1,8 @@
+#pragma once
+
 extern "C" void A64HookFunction(void *const symbol, void *const replace, void **result);
+extern "C" void A64InlineHook(void* const symbol, void* const replace);
+
+struct InlineCtx {
+    nn::os::CpuRegister registers[29];
+};
